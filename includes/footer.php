dec8891d8aeba388717fdd140c822ba4ab184ce5
Sakthi Aishwarya.S <67967960+SakthiAishwarya@users.noreply.github.com>
2021-08-20 14:44:08 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<div class="container">
+		<center>Copyright <span class="glyphicon glyphicon-copyright-mark"></span> <script type="text/javascript">var year = new Date();document.write(year.getFullYear());</script>
+		 Axis Bank.
+		All Rights Reserved | Contact Us: +91 90000 00000</center>
+	</div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<link href="style.css">
+<nav class="nav navbar-inverse navbar-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavBar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<div class="logo">
+			<a href="index.php" class="navbar-brand"><img src="logo.png" ></a>
+        </div> 
+		</div>
+		
+		<div class="collapse navbar-collapse" id="mynavBar">
+		
+				<div class="menu">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="customer.php">Customers</a></li>
+				    <li><a href="transfermoney.php">Transaction</a></li>
+					<li><a href="transactionhistory.php">History</a></li>
+					
+				</ul>
+				<span class="space"></span>
+				<span class="sce"></span>
+				<button type="button" class="btn-success"><a href="transfermoney.php">Transfer</a></button>
+    </nav>	
+    
+				
+        </div>
+		<div class="marq">
+    <marquee direction="left" scrolldelay=1> WELCOME TO Axis Bank. </marquee>
+	</div><br>		
+		
+				
+			
+		</div>
+	</div>
+</nav>
